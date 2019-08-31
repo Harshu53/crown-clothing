1c30dc5626867d55d256776afa3456a7a50b0ed9
Harshu53 <harshalmahajan53@gmail.com>
2019-08-31 19:22:15 +0530

    divded into components and some css work

---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import DirectoryMenu from "../../components/direactorymenu/directorymenu.component";
+import './homepage.styles.scss';
+
+const Homepage = () => {
+    return (
+        <div className="homepage">
+           <DirectoryMenu />
+        </div>
+    )
+}
+
+export default Homepage;
